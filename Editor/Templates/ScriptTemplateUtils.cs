fccd56f41b58bcc1a91e077430a99f394f7bde62
Sam Stamp <stampsam@outlook.com>
2024-09-18 14:51:57 +0100

    Altered where script templates load in packages

--- Editor/Templates/ScriptTemplateUtils.cs
+++ Editor/Templates/ScriptTemplateUtils.cs
@@ -8,20 +8,14 @@ using UnityEngine;
 /// Utility class for creating new scripts.
 /// </summary>
 public static class ScriptTemplateUtils {
-     
-     /// <summary>
-     /// 
-     /// </summary>
-     /// <param name="templatePath"></param>
-     /// <param name="fileName"></param>
-     private static void CreateScript(string templatePath, string fileName) {
+    private static void CreateScript(string templatePath, string fileName) {
          ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, fileName);
      }
      
-     [MenuItem("Assets/Create/Script/Monobehaviour", false, 10)]
-        private static void CreateMonoBehaviour() {
-            CreateScript("Assets/TG Utilities/Editor/Templates/MonoBehaviourTemplate.cs.txt", "NewMonoBehaviour.cs");
-        }
+     [MenuItem("Assets/Create/Script/Monobehaviour", false, 999)]
+     private static void CreateMonoBehaviour() {
+            CreateScript("Packages/com.stimpae.tgutilities/Editor/Templates/MonoBehaviourTemplate.cs.txt", "NewMonoBehaviour.cs");
+     }
      
      
 }
